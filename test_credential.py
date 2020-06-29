af6c075599e21fd9b80ce43e374ef940fa20dfee
Kenn <sabatojnr01@gmail.com>
2020-06-29 23:23:05 +0300

    modified test_credential.py

--- test_credential.py
+++ test_credential.py
@@ -6,8 +6,6 @@ class TestCredential(unittest.TestCase):
     '''
     Test class that defines test cases for the credential class behaviours.
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
     '''
     def setUp(self):
         '''

71265a8d499eb27c311517c174f880a22a0a0fea
Kenn <sabatojnr01@gmail.com>
2020-07-02 17:51:56 +0300

    added the behaviours

--- run.py
+++ run.py
@@ -11,14 +11,32 @@ def create_user(username,password):
     new_user = User(username,password) 
     new_user.save_user()
 
- 
+def create_credential(name):
+    """
+    function is to create the credentials of the user
+    """
+    new_credential = Credential(name)
+    new_credential.new_credential
+
 
 def main():
-    print("Please enter your name")
+    print("Hello and Welcome...Please enter your name")
     n_user=input()
     print("\n")
-    print("Please enter your password")
+    print(f"Hello {n_user}...Please enter your password")
     n_password = input()
+    print("Enter the your preferable username for your account ")
+    name = input()
+    print("Please enter the website name...")
+    w_name = input()
+    print("Please enter password...")
+    pcode = input()
+    print("Would you like the credentials to be displayed:Y/N")
+    if input() == "Y":
+       list_credentials()
+
+    elif input() =="N":
+        exit()
 
     create_user(n_user,n_password)
 
